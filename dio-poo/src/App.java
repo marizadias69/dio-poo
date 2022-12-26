6d2d15645db9d24edefc6f9538278d988801c697
marizadias69 <marizadias69@gmail.com>
2022-12-26 14:44:08 -0300

    Desafio de Projeto: Abstraindo um Bootcamp Usando POO

---
+++ dio-poo/src/App.java
@@ -0,0 +1,5 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Desafio de Projeto: Abstraindo um Bootcamp Usando Orientação a Objetos em Java");
+    }
+}
